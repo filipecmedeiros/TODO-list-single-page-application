9195bd0c7874da6f82840a2d4311a34e9114ef6b
Filipe Medeiros <fcma@cin.ufpe.br>
2018-03-16 17:25:38 -0300

    Criando modelo e personalização no django admin para ToDo list e Task

--- core/models.py
+++ core/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class ToDo (models.Model):
+	
+	name = models.CharField('Nome', max_length=200)
+	slug = models.SlugField('Identificador', max_length=200)
+
+	created = models.DateTimeField('Criado em', auto_now_add=True)
+	modified = models.DateTimeField('Modificado em', auto_now=True)
+
+	class Meta:
+		verbose_name='A fazer'
+		verbose_name_plural='A fazeres'
+		ordering=['modified']
+
+	def __str__(self):
+		return self.name
+
+class Task (models.Model):
+
+	name = models.CharField('Tarefa', max_length=200)
+	done = models.BooleanField('Feito', default=False)
+	todo = models.ForeignKey('core.ToDo', on_delete=models.CASCADE, verbose_name='A fazer')
+	deadline = models.DateTimeField('Data limite')
+
+	created = models.DateTimeField('Criado em', auto_now_add=True)
+	modified = models.DateTimeField('Modificado em', auto_now=True)
+
+	class Meta:
+		verbose_name='Tarefa'
+		verbose_name_plural='Tarefas'
+		ordering=['modified']
+
+	def __str__ (self):
+		return self.name
