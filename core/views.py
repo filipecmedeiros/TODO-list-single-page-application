cc9c2b79b391b7d8853ca9b3294cc57d7aec8053
Filipe Medeiros <fcma@cin.ufpe.br>
2018-03-19 20:34:39 -0300

    Listando todo e task

--- core/views.py
+++ core/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from .models import ToDo, Task
 # Create your views here.
 def index (request):
-	return render(request, 'index.html')
+	context = {
+		'todos' : ToDo.objects.all(),
+		'tasks' : Task.objects.all(),
+	}
+	return render(request, 'index.html', context)
